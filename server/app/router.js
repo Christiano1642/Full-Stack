3762a15298a5fa30587d3ad1fb8ec57e0dc4f9c5
Christian Wallace <christian.wallace@student.swic.edu>
2021-10-20 20:43:50 -0500

    create product

--- server/app/router.js
+++ server/app/router.js
@@ -1,6 +1,8 @@
 import Router from "express";
-import client from "./db/comms/clients.js";
 import config from "../config.js";
+import client from "./db/comms/clients.js";
+
+const collection = client.db(config.db.name).collection(config.db.collection)
 
 const router = new Router();
 const {
@@ -15,6 +17,19 @@ router.get("/products", async (_, res) => {
   res.json(products);
 });
 
-router.get("/pr")
+router.get("/products", async(_,res) =>) {
+  const products = await collection.find().toArray();
+  res.json(products);
+});
 
+
+router.get("/products/:id", async (req, res) =>{
+  const product = await collection.findOne({_id: ObjectId(req.params.id)});
+  res.json(product);
+});
+
+router.post("/products", async (req, res) =>{
+  const createdProduct = await collection.insertOne(req.body)
+  res.json(createdProduct);
+}
 export default router;
