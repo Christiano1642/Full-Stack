aea90d727054725e1c88ef725971e2ff11b27879
Christian Wallace <christian.wallace@student.swic.edu>
2021-11-03 19:49:06 -0500

    changed collection name

--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,13 +1,13 @@
 import once from "./comms/once.js";
-import products from "./data.js";
+import productsData from "./data.js";
 import config from "../config.js";
 
 const {
-  db: { name, collectionName: collection },
+  db: { name,  collection },
 } = config;
 (async () => {
   const conn = await once.connect();
   await conn.db(name).collection(collection).deleteMany({});
-  await conn.db(name).collection(collection).insertMany(products);
+  await conn.db(name).collection(collection).insertMany(productsData);
   conn.close();
 })();
